7f70819e8110eebdc3618817be437224d0d9d00d
Ryan <119572870+RyanKhangTruong@users.noreply.github.com>
2024-07-01 02:10:08 -0500

    Main Python file that operates the practice window
    This file creates a window and displays a random Cangjie character component. The user must press the correct corresponding key for the program to move on to the next component, which it will choose randomly from the cangjie_shapes folder.

---
+++ cangjie_practice.py
@@ -0,0 +1,47 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from os import *
+from random import *
+from time import sleep
+
+root = Tk()
+root.title("Cangjie Practice")
+
+def choose_shape():
+    chdir("cangjie_shapes")
+    # Get list of letters
+    letter_list = listdir()
+    if ".DS_Store" in letter_list: letter_list.remove(".DS_Store")
+    if "cangjie_archive" in letter_list: letter_list.remove("cangjie_archive")
+    # Choose a letter
+    letter = choice(letter_list)
+    chdir(letter)
+    # Get a list of shapes
+    shape_list = listdir()
+    if ".DS_Store" in shape_list: shape_list.remove(".DS_Store")
+    # Choose a shape
+    shape = choice(shape_list)
+    # Exit directory twice
+    chdir(pardir)
+    chdir(pardir)
+
+    return f'cangjie_shapes/{letter}/{shape}'
+
+shape_path = choose_shape()
+shape_name = shape_path.split("/")[-1][0]
+open_img = ImageTk.PhotoImage(Image.open(shape_path))
+disp_img = Label(root, image = open_img)
+disp_img.pack(side="top", fill="both", expand="yes")
+
+def callback(event):
+    global shape_name
+    if event.keysym == shape_name:
+        shape_path = choose_shape()
+        shape_name = shape_path.split("/")[-1][0]
+        new_img = ImageTk.PhotoImage(Image.open(shape_path))
+        disp_img.configure(image = new_img)
+        disp_img.image = new_img
+
+root.bind('<Key>', callback)
+
+root.mainloop()
